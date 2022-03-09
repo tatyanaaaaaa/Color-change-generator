96b8f5fa08255167da612ccec14e2a6fc4e96e4a
Sosina Tatyana <fedorova_t95@mail.ru>
2022-03-09 14:17:15 +0300

    Initial Commit

---
+++ Color change generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color change generator
+//
+//  Created by Tatiana Sosina on 09.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

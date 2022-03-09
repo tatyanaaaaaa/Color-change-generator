42818ad822656bb76c474ba277434004d3100569
Sosina Tatyana <fedorova_t95@mail.ru>
2022-03-09 15:13:26 +0300

    Add logic change color for slider

--- Color change generator/ViewController.swift
+++ Color change generator/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBOutlet weak var redChangeValueLabel: UILabel!
+    @IBOutlet weak var greenChangeValueLabel: UILabel!
+    @IBOutlet weak var blueChangeValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundView.layer.cornerRadius = 21
+        changeBackgrounColorView()
     }
 
-
+    @IBAction func changeColorSlider() {
+        setDefaultSettingsChangeColor()
+    }
+    
+    private func changeBackgrounColorView() {
+        setDefaultSettingsChangeColor()
+    }
+    
+    private func setDefaultSettingsChangeColor() {
+        redChangeValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenChangeValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueChangeValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        backgroundView.backgroundColor = .init(red: CGFloat(redSlider.value),
+                                               green: CGFloat(greenSlider.value),
+                                               blue: CGFloat(blueSlider.value),
+                                               alpha: 1)
+    }
+    
+    
+    
+    
 }
 
